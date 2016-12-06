c5e2d87c93dbb3196f13ae2a8284feebc4834ff6
Stanley Ye <stanleyye1996@gmail.com>
2016-12-05 22:22:25 -0800

    formatting

--- 2016/day5part1.java
+++ 2016/day5part1.java
@@ -1,35 +1,33 @@
-import java.lang.Math;
+import java.io.*;
+import java.util.*;
 
-public class day5part1 {
-  public static void main(String[] args) throws Exception {
-    String input = "uqwqemis";
-    String temp = input;
-    String res = "";
-    int index = 0;
-    for (int x = 0; x < input.length(); x++) {
-        while (!temp.startsWith("00000")) {
-            temp += Integer.toString(index);
-            temp = new String(day5part1.MD5(temp));
-            //System.out.println(temp);
-        }
-      	//System.out.println(temp);
-        if (temp != null) {
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String input = "uqwqemis";
+        String res = "";
+        int index = 0;
+        
+        for (int x = 0; x < input.length(); x++) {
+            String temp = input + Integer.toString(index);
+            while (!temp.startsWith("00000")) {
+                temp = Main.MD5(input + Integer.toString(index));
+                index++;
+            }
+            index++;
             res += Character.toString(temp.charAt(5));
         }
+        System.out.println(res);
     }
-    System.out.println(res);
-  }
+    public static String MD5(String md5) throws Exception {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+            }
+            return sb.toString();
+        } finally {}
 
-  public static String MD5(String md5) throws Exception {
-     try {
-          java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
-          byte[] array = md.digest(md5.getBytes());
-          StringBuffer sb = new StringBuffer();
-          for (int i = 0; i < array.length; ++i) {
-            sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
-         }
-          return sb.toString();
-     } finally {}
-
-  }
+    }
 }
